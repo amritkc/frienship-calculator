5045ad23f57b7bb75ad72357f4dded1af5f1995a
unknown <amrit.khdk.me@gmail.com>
2019-10-06 11:31:00 +0530

    Calculator

---
+++ Frienship-calculator.py
@@ -0,0 +1,130 @@
+
+def game():
+    print("\n")
+    print("F R I N D I S H I P  C A L C U L A T O R \n")
+
+    var1 = input("Enter Your Name :")
+    if "a" == var1[0] or "A" == var1[0]:
+        print("\nCool! Name")
+    if "b" == var1[0] or "B" == var1[0]:
+        b = var1.upper()
+        print("\nB for " + b + " isn't cool?  by the way nice Name :)" )
+    if "c" == var1[0] or "C" == var1[0]:
+        c = var1.upper()
+        print("\nChenni express = " +c+ " nice joke  nice name! ")
+    if "d" == var1[0] or "D" == var1[0]:
+        d = var1.upper()
+        print("\ndhooom machalay...... " +d+ " machalay..... dhoom dhoom ")
+    if "e" == var1[0] or "E" == var1[0]:
+        e = var1.upper()
+        print("\n "+ e + "simple name  then must be simple person ")
+    if "f" == var1[0] or "F" == var1[0]:
+        f = var1.upper()
+        print("\n "+ f + "Are you a Cruel Person? no right!!! ")
+    if "g" == var1[0] or "G" == var1[0]:
+        g = var1.upper()
+        print("\n"+ g + "Gaignantic Person ")
+    if "h" == var1[0] or "H" == var1[0]:
+        h = var1.upper()
+        print("\n " + h + " Do you mean chutiya")
+    if "i" == var1[0] or "I" == var1[0]:
+        i = var1.upper()
+        print("\nI Think  You Are Inovative Thinker! Am i Right  " +i+ "?")
+    if "j" == var1[0] or "J" == var1[0]:
+        j = var1.upper()
+        print("\nHello " +j+" are you male or female? i'm confused!!" )
+    if "k" == var1[0] or "K" == var1[0]:
+        k = var1.upper()
+        print("\n "+ k + "I Love the Name that starts with letter K ")
+    if "l" == var1[0] or "L" == var1[0]:
+        l = var1.upper()
+        print("\n "+ l + " Lover boy vadaenga!! it's Tamil word!")
+    if "m" == var1[0] or "m" == var1[0]:
+        m = var1.upper()
+        print("\nmental is here " + m +  " is mental")
+    if "n" == var1[0] or "N" == var1[0]:
+        n = var1.upper()
+        print("\n Want Job The Become ExPert in Python" +n)
+    if "o" == var1[0] or "O" == var1[0]:
+        o = var1.upper()
+        print("\nChenni express = " +o+ " nice joke  nice name! ")
+    if "p" == var1[0] or "P" == var1[0]:
+        p = var1.upper()
+        print("\n " +p+ " no comments ")
+    if "q" == var1[0] or "Q" == var1[0]:
+        q = var1.upper()
+        print("\nChenni express = " +q+ " nice joke  nice name! ")
+    if "r" == var1[0] or "R" == var1[0]:
+        r = var1.upper()
+        print("\n " +r+ " are you mental? nice name ")
+    if "s" == var1[0] or "S" == var1[0]:
+        s = var1.upper()
+        print("\nChenni express = " +s+ " nice joke  nice name! ")
+    if "t" == var1[0] or "T" == var1[0]:
+        t = var1.upper()
+        print("\nChenni express = " +t+ " nice joke  nice name! ")
+    if "u" == var1[0] or "U" == var1[0]:
+        u = var1.upper()
+        print("\nChenni express = " +u+ " nice joke  nice name! ")
+    if "v" == var1[0] or "V" == var1[0]:
+        v = var1.upper()
+        print("\n"+ v + "Man all the best!! ")
+    if "w" == var1[0] or "W" == var1[0]:
+        w = var1.upper()
+        print("\n "+ w + "Dude Change your Name!!")
+    if "x" == var1[0] or "X" == var1[0]:
+        x = var1.upper()
+        print("\n oww!! okkward Name!!  and " +x + " who kept your name?")
+    if "y" == var1[0] or "Y" == var1[0]:
+        y = var1.upper()
+        print( "\n" + y + " your name says you are too naughty!!! ")
+    if "z" == var1[0] or "Z" == var1[0]:
+        z = var1.upper()
+        print("\n Z for Zebra and " +z + ", You are lucky your name starts from Z\n")
+    friend = input("\nEnter Your Friend Name: ")
+    print("Your Friend name is "+ friend+"." )
+    name2 = var1[1]
+    fname2 = friend[1]
+    name3 = var1[0]
+    fname3 = friend[2]
+    name4 = var1[2]
+    fname4 = friend[3]
+    name5 = var1[0]
+    fname5 = friend[4]
+    name6 = var1[4]
+    fname6 = friend[5]
+    name7 = var1[1]
+    fname7 = friend[0]
+    number1 = var1[0]
+    number2 = friend[0]
+    if number1 == number2 :
+        per = 96
+    elif name2 == fname2:
+        per = 62
+    elif name3 == fname3:
+        per = 72
+    elif name4 == fname4:
+        per = 83
+    elif name5 == fname5:
+        per = 99
+    elif name6 == fname6:
+        per = 79
+    elif name7 == fname7:
+        per = 89 
+    else:
+        per = 56
+    print("..................................................................................")
+    print(".    ...................................................................         .")
+    print(".         ...................................................                    .")
+    print(".         ACCORDING TO  PYTHON SCRIPT YOUR FRIENSHIP LEVEL IS                    .")
+    print(".                              "+str(per)+"%                                               .")
+    print("..................................................................................")
+    print("")
+    print("                                                          Developer - Amrit Khadka")
+    
+        
+           
+
+        
+
+game()
